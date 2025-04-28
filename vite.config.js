c7f67ab2d5f5d7747ed179daef1aeb985e42536e
Gokul <gokulakrishnanoffl@gmail.com>
2025-04-28 10:35:57 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  build:{
-    outDir: 'build',
+  base: "/TaskNest/", // Set the base path for GitHub Pages
+  build: {
+    outDir: "build", // Ensure the build output is correct
   },
   plugins: [react()],
-})
+});
