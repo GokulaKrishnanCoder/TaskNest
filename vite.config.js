8ea258e26649c10db14682f095053f3b60b6364e
Gokula Krishnan <gokulakrishnanoffl@gmail.com>
2025-05-10 16:36:10 +0530

    change6

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  // base: "/TaskNest/", // Set the base path for GitHub Pages
-  // build: {
-  //   outDir: "build", // Ensure the build output is correct
-  // },
+  base: '', // ✅ make sure it's empty
   plugins: [react()],
-  base: '',
-});
+})
